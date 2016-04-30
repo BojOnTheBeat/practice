61d63a2cd69b45f955e9f264739cfcc9a56be0d6
Bolaji Fatade <bjfatade@gmail.com>
2016-04-30 17:44:29 -0400

    Did fizzbuzz

---
+++ text/fizzbuzz.py
@@ -0,0 +1,23 @@
+# FizzBuzz test
+
+# Write a program than prints numbers from 1 to 100 and if the number is a multiple of 3, prints "Fizz"
+# If the number is a multiple of 5, prints buzz and if the number is a multiple of both, prints fizzbuzz
+
+
+
+def fizzbuzz():
+	for number in range(100):
+		if ((number % 3 == 0) and (number % 5 == 0)):
+			print "FizzBuzz"
+		elif number % 3 == 0:
+			print "Fizz"
+		elif number % 5 == 0:
+			print "Buzz"
+		else:
+			print number
+
+
+if __name__ == '__main__':
+	fizzbuzz()
+
+
