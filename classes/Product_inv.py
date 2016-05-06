ab43a10e87bcf0a033c00098490d35d45c107592
Bolaji Fatade <bjfatade@gmail.com>
2016-05-06 14:51:33 -0400

    Finsih product_inv

--- classes/Product_inv.py
+++ classes/Product_inv.py
@@ -7,19 +7,64 @@ class product:
 		self.ID = ID
 		self.qty = qty
 
+	def get_price(self):
+		return self.price
+
+	def set_price(self, new_price):
+		self.price = new_price
+
+	def get_qty(self):
+		return self.qty
+
+	def set_qty(self, new_qty):
+		self.qty = new_qty
+
 
 
 
 class inventory:
-	"""docstring for ClassName"""
+	"""docstring for inventory class"""
 	def __init__(self,):
 		self.prodList = []
 
 	def sum_inv_value():
 		total = 0
 		for product in self.prodList:
-			total += product.price
+			total += product.get_price
 		return total
+
+	def add_product(self, product):
+		self.prodList.append(product)
+
+	def remove_product(self, prod_ID):
+		for product in self.prodList:
+			if product.ID is prod_ID:
+				self.prodList.remove(product)
+
+	def view_inventory(self):
+		return self.prodList
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 		
