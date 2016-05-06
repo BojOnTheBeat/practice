0ef65949926977ce29ce197e14d600148578191a
Bolaji Fatade <bjfatade@gmail.com>
2016-05-05 23:51:10 -0400

    Do sum

--- classes/Product_inv.py
+++ classes/Product_inv.py
@@ -1,10 +1,25 @@
 
 
-class product():
+class product:
 	def __init__(self, name, price, ID, qty):
 		self.name = name
 		self.price = price
 		self.ID = ID
 		self.qty = qty
+
+
+
+
+class inventory:
+	"""docstring for ClassName"""
+	def __init__(self,):
+		self.prodList = []
+
+	def sum_inv_value():
+		total = 0
+		for product in self.prodList:
+			total += product.price
+		return total
+
 
 		
